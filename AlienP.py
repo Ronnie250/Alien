6f5a64ce5a1285d85a1061821d9ff42cdd655f7b
Ronnie250 <Ronniemelwani@gmail.com>
2024-11-04 19:36:14 +0000

    Add files via upload

--- AlienP.py
+++ AlienP.py
@@ -5,20 +5,37 @@ a=Actor("alien")
 a.x=400
 a.y=250
 msg="Catch the alien"
+hits=0
+misses=0
+game_over=False
+score=0
 
 def draw():
     screen.fill("#010029")
     a.draw()
     screen.draw.text(msg,(330,330))
+    screen.draw.text("Hits="+str(hits),(10,10))
+    screen.draw.text("Misses="+str(misses),(715,10))
+    if game_over:
+        screen.fill("#010029")
+        screen.draw.text(str(score), (330, 330), fontsize=100)
+
+def time_up():
+    global score, hits, misses, game_over
+    game_over=True
+    score=hits-misses
 
 def on_mouse_down(pos):
-    global msg
+    global msg, misses, hits
     if a.collidepoint(pos):
         a.x=random.randint(0,800)
         a.y=random.randint(0,500)
+        hits=hits+1
         msg="Good Shot!"
     else:
         msg="You Missed!"
+        misses=misses+1
 
+clock.schedule(time_up, 10.0)
 
 pgzrun.go()
